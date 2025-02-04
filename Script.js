9d258f583ffcf8fccc34629f1c00ed259ec61ea1
Prabhat Seth <prabhat.pkr012@gmail.com>
2025-02-04 17:02:13 +0530

    Second commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+let con = document.querySelector("#container");
+let like = document.querySelector("i");
+con.addEventListener("dblclick", function () {
+    like.style.transform = "translate(-50%,-50%) scale(1)";
+    like.style.opacity = 0.8;
+    setTimeout(function () {
+        like.style.opacity = 0;
+
+    },1000);
+    setTimeout(function () {
+        like.style.transform = "translate(-50%,-50%) scale(0)";
+
+    },2000);
+
+});
+
